3104fd9c7283333c3504567a6a24bf57faf89b85
Gumowska <gumowska.agata@wp.pl>
2018-06-08 14:23:19 +0200

    Dodanie losowo pojawiających się bonusów

--- SnakeGame/SnakeGame/Form1.cs
+++ SnakeGame/SnakeGame/Form1.cs
@@ -13,7 +13,7 @@ namespace SnakeGame
     public partial class frmSnake : Form
     {
         Random rand;
-        enum GameBoardField
+        enum GameBoardFields
         {
             Free,
             Snake,
@@ -35,18 +35,18 @@ namespace SnakeGame
         };
 
 
-        GameBoardField[,] gameBoardField;
+        GameBoardFields[,] gameBoardField;
         SnakeCoordinates[] snakeXY;
         int snakeLength;
         Directions direction;
         Graphics g;
 
-       
+
 
         public frmSnake()
         {
             InitializeComponent();
-            gameBoardField = new GameBoardField[11, 11];
+            gameBoardField = new GameBoardFields[11, 11];
             snakeXY = new SnakeCoordinates[100];
             rand = new Random();
         }
@@ -57,7 +57,7 @@ namespace SnakeGame
             g = Graphics.FromImage(picGameBoard.Image);
             g.Clear(Color.White);
 
-            for (int i=1; i<=10; i++)
+            for (int i = 1; i <= 10; i++)
             {
                 //górna dolna ściana
                 g.DrawImage(imgList.Images[6], i * 35, 0);
@@ -83,17 +83,34 @@ namespace SnakeGame
             g.DrawImage(imgList.Images[4], 5 * 35, 6 * 35); // pierwsza część tułowia
             g.DrawImage(imgList.Images[4], 5 * 35, 7 * 35); // druga część tułowia
 
-            gameBoardField[5, 5] = GameBoardField.Snake;
-            gameBoardField[5, 6] = GameBoardField.Snake;
-            gameBoardField[5, 7] = GameBoardField.Snake;
+            gameBoardField[5, 5] = GameBoardFields.Snake;
+            gameBoardField[5, 6] = GameBoardFields.Snake;
+            gameBoardField[5, 7] = GameBoardFields.Snake;
 
             direction = Directions.Up;
             snakeLength = 3;
 
-            for(int i = 0; i<4; i++)
+            for (int i = 0; i < 4; i++)
             {
                 Bonus();
             }
+        }
+
+     private void Bonus()
+        {
+        int x, y;
+        var imgIndex = rand.Next(0, 4);
+
+            do
+            {
+                x = rand.Next(1, 10);
+                y = rand.Next(1, 10);
+            }
+
+            while (gameBoardField[x, y] != GameBoardFields.Free);
+
+            gameBoardField[x, y] = GameBoardFields.Bonus;
+            g.DrawImage(imgList.Images[imgIndex], x * 35, y * 35);
         }
     }
 }
